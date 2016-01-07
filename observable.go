f48f5cb5e43a2cfe9e12becf170e3ba335f76532
bryn austin bellomy <bryn.bellomy@gmail.com>
2016-01-07 13:07:22 -0600

    Adding Observable

--- observable.go
+++ observable.go
@@ -4,16 +4,55 @@ type Observable struct {
 	OnSubscribe func() (IObservable, IDisposable)
 }
 
+func ObservableFrom(subscribable ISubscribable) *Observable {
+	return &Observable{subscribable.Subscribe}
+}
+
+func (o *Observable) Take(n int) *Observable {
+	return &Observable{func() (IObservable, IDisposable) {
+		chIn, cancelIn := o.Subscribe()
+		t := NewTake(n)
+
+		go func() {
+			defer cancelIn.Cancel()
+			defer t.Complete()
+
+			for x := range chIn.Out() {
+				t.Send(x)
+			}
+		}()
+		return t.Subscribe()
+	}}
+}
+
+func (o *Observable) Broadcast() *Broadcaster {
+	chIn, cancelIn := o.Subscribe()
+	b := NewBroadcaster()
+
+	go func() {
+		defer cancelIn.Cancel()
+		defer b.Complete()
+
+		for x := range chIn.Out() {
+			b.Send(x)
+		}
+	}()
+
+	return b
+}
+
 func (o *Observable) Batch(size int) *Observable {
 	return &Observable{func() (IObservable, IDisposable) {
 		chIn, cancelIn := o.Subscribe()
 		b := NewBatcher(size)
+
 		go func() {
 			defer cancelIn.Cancel()
+			defer b.Complete()
+
 			for x := range chIn.Out() {
 				b.Send(x)
 			}
-			b.Complete()
 		}()
 		return b.Subscribe()
 	}}
@@ -24,14 +63,15 @@ func (o *Observable) Map(tfm func(x interface{}) (interface{}, error)) *Observab
 		func() (IObservable, IDisposable) {
 			chIn, cancelIn := o.Subscribe()
 			m := NewMap(tfm)
+
 			go func() {
 				defer cancelIn.Cancel()
+
 				for x := range chIn.Out() {
 					m.Send(x)
 				}
 				m.Complete()
 			}()
-
 			return m.Subscribe()
 		},
 	}
--- observable_test.go
+++ observable_test.go
@@ -1,6 +1,8 @@
 package rx_test
 
 import (
+	"sync"
+
 	"github.com/brynbellomy/go-result"
 	"github.com/brynbellomy/go-rx"
 	τ "gopkg.in/check.v1"
@@ -10,13 +12,43 @@ type observableSuite struct{}
 
 var _ = τ.Suite(&observableSuite{})
 
+func (s *observableSuite) TestBroadcastObservableSliceGenerator(c *τ.C) {
+	const BATCH_SIZE = 3
+
+	sl := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	gen := &rx.SliceGenerator{sl}
+
+	b := gen.AsObservable().Broadcast()
+
+	out1, _ := b.Subscribe()
+	out2, _ := b.Subscribe()
+
+	subscribers := []rx.IObservable{out1, out2}
+
+	wait := sync.WaitGroup{}
+	wait.Add(len(subscribers))
+
+	for _, out := range subscribers {
+		go func(out rx.IObservable) {
+			i := 0
+			for x := range out.Out() {
+				c.Assert(x.Value(), τ.Equals, sl[i])
+				i++
+			}
+			c.Assert(i, τ.Equals, len(sl))
+			wait.Done()
+		}(out)
+	}
+	wait.Wait()
+}
+
 func (s *observableSuite) TestBatchObservableSliceGenerator(c *τ.C) {
 	const BATCH_SIZE = 3
 
 	sl := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	gen := &rx.SliceGenerator{sl}
 
-	out, cancel := gen.AsObservable().Batch(BATCH_SIZE).Subscribe()
+	out, _ := gen.AsObservable().Batch(BATCH_SIZE).Subscribe()
 
 	i := 0
 	for batch := range out.Out() {
@@ -28,8 +60,6 @@ func (s *observableSuite) TestBatchObservableSliceGenerator(c *τ.C) {
 			i++
 		}
 	}
-
-	cancel.Cancel()
 }
 
 func (s *observableSuite) TestMapObservableSliceGenerator(c *τ.C) {
@@ -38,7 +68,7 @@ func (s *observableSuite) TestMapObservableSliceGenerator(c *τ.C) {
 	sl := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	gen := &rx.SliceGenerator{sl}
 
-	out, cancel := gen.AsObservable().Map(func(x interface{}) (interface{}, error) {
+	out, _ := gen.AsObservable().Map(func(x interface{}) (interface{}, error) {
 		return x.(int) * 2, nil
 	}).Subscribe()
 
@@ -48,5 +78,4 @@ func (s *observableSuite) TestMapObservableSliceGenerator(c *τ.C) {
 		i++
 	}
 
-	cancel.Cancel()
 }
